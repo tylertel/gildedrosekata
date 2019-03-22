7a3453698527b86fd52771ed4db93bba641facaf
QUALFONCENTER\SJeelani <sjeelani@qualfon.com>
2019-03-22 18:54:32 +0530

    fixed basic app unit tests
    fixed application unit tests + added two new unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        FormsModule
       ],
     }).compileComponents();
   }));
@@ -22,6 +26,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to GildedRose!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Gilded Rose Kata!');
   }));
+
+  // it('min days should be greater than 1', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const component=fixture.componentInstance;
+  //   expect(component.days).toBeGreaterThanOrEqual(2);
+  // }));
 });
--- src/app/item.spec.ts
+++ src/app/item.spec.ts
@@ -1,10 +1,23 @@
 import { Item } from "./item";
+import { async } from "q";
 
 describe('Item', function () {
 
     it('should foo', function() {
         const item = new Item('foo', 0, 0);
-        expect(item.name).toEqual('fixme');
+        expect(item.name).toEqual('foo');
+    });
+
+    //The Quality of an item is never negative
+    it(`item quality can't be -ve`, function() {
+        const item=new Item('foo',5,1);
+        expect(item.quality).toBeGreaterThanOrEqual(0);
+    });
+
+    //The Quality of an item is never more than 50
+    it(`item quality can't be greater than 50`, function() {
+        const item=new Item('foo',5,50);
+        expect(item.quality).toBeLessThanOrEqual(50);
     });
 
 });
