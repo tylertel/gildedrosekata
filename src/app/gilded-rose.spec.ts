25754ec6d539060ff25c8509b527e5bd4483022d
tyler tel <ttel@qualfon.com>
2019-04-04 10:50:33 -0400

    added placeholder for other tests.

--- src/app/gilded-rose.spec.ts
+++ src/app/gilded-rose.spec.ts
@@ -12,7 +12,73 @@ describe("Gilded Rose", function() {
     service.initiateGildedRose(periodInDays);
   });
 
-  it("BackStage passes, should be of zero Quality, where SellIn value is -ve", function() {
+  //   it("Once the sell by date has passed, Quality degrades twice as fast", function() {
+  //     expect(true).toBe(false);
+  //   });
+
+  it("The Quality of an item is never negative", function() {
+    let foundItems = 0;
+    service.dayLog.forEach(day => {
+      let items = day.items;
+      foundItems = foundItems + items.filter(r => r.quality < 0).length;
+    });
+
+    expect(foundItems).toEqual(0);
+  });
+
+  //"Aged Brie" actually increases in Quality the older it gets
+
+  it("The Quality of an item is never more than 50 * can't increase to over 50 see sulfuras", function() {
+    let countOfItemsWithQualityGreaterThan50 = 0;
+
+    service.dayLog.forEach(day => {
+      let items = day.items;
+
+      let ItemsWithQualityGreaterThan50 = items.filter(r => r.quality > 50);
+      ItemsWithQualityGreaterThan50.forEach(itemOver50 => {
+        let ItemStartingQualityValue = service.dayLog[0].items.find(
+          x => x.name == itemOver50.name
+        ).quality;
+        if (itemOver50.quality > ItemStartingQualityValue) {
+          countOfItemsWithQualityGreaterThan50++;
+        }
+      });
+    });
+
+    expect(countOfItemsWithQualityGreaterThan50).toEqual(0);
+  });
+
+  //   it("Sulfuras, being a legendary item, never has to be sold", function(){
+  //     expect(true).toBe(false);
+  //   })
+
+  it("Sulfuras, being a legendary item, never decreases in Quality", function() {
+    let sulfurasStartingQuality = service.dayLog[0].items.filter(
+      f => (f.name = "Sulfuras, Hand of Ragnaros")
+    ).quality;
+    let countOfQualityDecreases = 0;
+    service.dayLog.forEach(day => {
+      //this can easily be changed to quality never changed instead of quality never decreases.
+      if (
+        day.items.filter(f => (f.name = "Sulfuras, Hand of Ragnaros")).quality <
+        sulfurasStartingQuality
+      ) {
+        countOfQualityDecreases++;
+      }
+    });
+
+    expect(countOfQualityDecreases).toBe(0);
+  });
+
+  it("BackStage passes: increases in Quality as its SellIn value approaches", function() {
+    expect(true).toBe(false);
+  });
+
+  it("BackStage passes: Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less", function() {
+    expect(true).toBe(false);
+  });
+
+  it("BackStage passes: Quality drops to 0 after the concert", function() {
     let daysPassed = service.dayLog.length;
 
     let passesWithMoreSellIn = service.dayLog[0].items.filter(f => {
@@ -40,51 +106,7 @@ describe("Gilded Rose", function() {
     expect(garbagePassesCount).toEqual(passesWithLessSellIn);
   });
 
-  it("quality can't be Negative", function() {
-    let foundItems = 0;
-    service.dayLog.forEach(day => {
-      let items = day.items;
-      foundItems = foundItems + items.filter(r => r.quality < 0).length;
-    });
+  //   it("Conjured items: degrade in Quality twice as fast as normal items",function(){
 
-    expect(foundItems).toEqual(0);
-  });
-
-  it("quality can't increase to be more than 50", function() {
-    let countOfItemsWithQualityGreaterThan50 = 0;
-
-    service.dayLog.forEach(day => {
-      let items = day.items;
-
-      let ItemsWithQualityGreaterThan50 = items.filter(r => r.quality > 50);
-      ItemsWithQualityGreaterThan50.forEach(itemOver50 => {
-        let ItemStartingQualityValue = service.dayLog[0].items.find(
-          x => x.name == itemOver50.name
-        ).quality;
-        if (itemOver50.quality > ItemStartingQualityValue) {
-          countOfItemsWithQualityGreaterThan50++;
-        }
-      });
-    });
-
-    expect(countOfItemsWithQualityGreaterThan50).toEqual(0);
-  });
-
-  it("Sulfuras, quality never decreases", function() {
-    let sulfurasStartingQuality = service.dayLog[0].items.filter(
-      f => (f.name = "Sulfuras, Hand of Ragnaros")
-    ).quality;
-    let countOfQualityDecreases = 0;
-    service.dayLog.forEach(day => {
-      //this can easily be changed to quality never changed instead of quality never decreases.
-      if (
-        day.items.filter(f => (f.name = "Sulfuras, Hand of Ragnaros")).quality <
-        sulfurasStartingQuality
-      ) {
-        countOfQualityDecreases++;
-      }
-    });
-
-    expect(countOfQualityDecreases).toBe(0);
-  });
+  //   })
 });
