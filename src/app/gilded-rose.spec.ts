863c1e35f3002e63c3e704fbd624d83715bb2a54
tyler tel <ttel@qualfon.com>
2019-04-04 10:14:12 -0400

    fixed specs for backstage passes and quality increase over 50

--- src/app/gilded-rose.spec.ts
+++ src/app/gilded-rose.spec.ts
@@ -1,74 +1,77 @@
-import { GildedRose } from '../app/gilded-rose';
-import { Item } from './item';
-import { TestBed } from '@angular/core/testing';
-import { GildedRoseService } from './gilded-rose.service';
+import { GildedRose } from "../app/gilded-rose";
+import { Item } from "./item";
+import { TestBed } from "@angular/core/testing";
+import { GildedRoseService } from "./gilded-rose.service";
 
 const periodInDays: number = 30;
-describe('Gilded Rose', function () {
+describe("Gilded Rose", function() {
+  TestBed.configureTestingModule({});
+  let service: any;
+  beforeAll(() => {
+    service = TestBed.get(GildedRoseService);
+    service.initiateGildedRose(periodInDays);
+  });
 
-    TestBed.configureTestingModule({});
-    let service: any;
-    debugger
-    beforeAll(() => {
-        service = TestBed.get(GildedRoseService);
-        service.initiateGildedRose(periodInDays)
-        let GildedRoseClass = service.gildedRose;
-    })
+  it("BackStage passes, should be of zero Quality, where SellIn value is -ve", function() {
+    let daysPassed = service.dayLog.length;
 
-    it('should foo', function () {
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual('foo');
+    let passesWithMoreSellIn = service.dayLog[0].items.filter(f => {
+      return (
+        f.name == "Backstage passes to a TAFKAL80ETC concert" &&
+        f.sellIn > periodInDays
+      );
+    }).length;
+
+    let passesWithLessSellIn = service.dayLog[0].items.filter(f => {
+      return (
+        f.name == "Backstage passes to a TAFKAL80ETC concert" &&
+        f.sellIn < periodInDays
+      );
+    }).length;
+
+    let latestSellInValue = service.dayLog[daysPassed - 1].items.filter(
+      f => f.name == "Backstage passes to a TAFKAL80ETC concert"
+    );
+
+    let garbagePassesCount = latestSellInValue.filter(
+      element => element.quality == 0
+    ).length;
+
+    expect(garbagePassesCount).toEqual(passesWithLessSellIn);
+  });
+
+  it("quality can't be Negative", function() {
+    let foundItems = 0;
+    service.dayLog.forEach(day => {
+      let items = day.items;
+      foundItems = foundItems + items.filter(r => r.quality < 0).length;
     });
 
-    it('BackStage passes, should be of zero Quality, where SellIn value is -ve', function () {
+    expect(foundItems).toEqual(0);
+  });
 
-        let daysPassed = service.dayLog.length;
+  it("quality can't increase to be more than 50", function() {
+    let countOfItemsWithQualityGreaterThan50 = 0;
+    
+    service.dayLog.forEach(day => {
+      let items = day.items;
 
-        let passesWithMoreSellIn = service.dayLog[0].items.filter(f => { return f.name == 'Backstage passes to a TAFKAL80ETC concert' && f.sellIn > periodInDays }).length;
-
-        let passesWithLessSellIn = service.dayLog[0].items.filter(f => { return f.name == 'Backstage passes to a TAFKAL80ETC concert' && f.sellIn < periodInDays }).length;
-
-        let latestSellInValue = service.dayLog[daysPassed - 1].items.filter(f => f.name == 'Backstage passes to a TAFKAL80ETC concert');
-
-        let garbagePassesCount = latestSellInValue.filter(element => element.quality == 0).length
-
-        expect(garbagePassesCount).toEqual(passesWithLessSellIn);
-
+      
+      let ItemsWithQualityGreaterThan50 = items.filter(r => r.quality > 50);
+      ItemsWithQualityGreaterThan50.forEach(itemOver50 => {
+        let ItemStartingQualityValue = service.dayLog[0].items.find(x => x.name == itemOver50.name).quality;
+        if(itemOver50.quality > ItemStartingQualityValue){
+            countOfItemsWithQualityGreaterThan50++
+        }
+      });    
     });
 
-    debugger;
-    it('quality can\'t be Negative', function () {
-        service.dayLog.forEach(element => {
+    expect(countOfItemsWithQualityGreaterThan50).toEqual(0);
 
-            let items = element.item;
-
-            let foundItem = items.filter(r => r.quality < 0).length;
-            debugger
-            expect(foundItem).toEqual(0);
-        })
-    })
-    it('quality can\'t be more than 50', function () {
-
-        service.dayLog.forEach(element => {
-
-            let items = element.items            
-
-            let foundItem =items.filter(r => r.quality >= 50).length;
-            debugger
-            expect(foundItem).toEqual(0)
-
-        })
-
-        // it('Sulfuras, quality never decreases',function(){
-        //     debugger
-        //     if(modifiedItems.filter(f => f.name = 'Sulfuras, Hand of Ragnaros')===SulphurusQuality){
-        //         expect('Sulfuras Quality is same');
-        //     }
-        // }
-
-
-    })
+    // it('Sulfuras, quality never decreases',function(){
+    //     if(modifiedItems.filter(f => f.name = 'Sulfuras, Hand of Ragnaros')===SulphurusQuality){
+    //         expect('Sulfuras Quality is same');
+    //     }
+    // }
+  });
 });
-
-
--- src/app/item.spec.ts
+++ src/app/item.spec.ts
@@ -3,21 +3,8 @@ import { async } from "q";
 
 describe('Item', function () {
 
-    it('should foo', function() {
+    it('creates new item', function() {
         const item = new Item('foo', 0, 0);
         expect(item.name).toEqual('foo');
     });
-
-    //The Quality of an item is never negative
-    it(`item quality can't be -ve`, function() {
-        const item=new Item('foo',5,1);
-        expect(item.quality).toBeGreaterThanOrEqual(0);
-    });
-
-    //The Quality of an item is never more than 50
-    it(`item quality can't be greater than 50`, function() {
-        const item=new Item('foo',5,50);
-        expect(item.quality).toBeLessThanOrEqual(50);
-    });
-
 });
